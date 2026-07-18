6e16ff8b763cd35614c75fb940f8a8862b8a54f5
Tharanirajan M <tharanirajantharani1610@gmail.com>
2026-07-18 21:27:27 +0530

    Create

---
+++ problem-1784390248147/version/java/problem-1784390248147_20260718_212728.java
@@ -0,0 +1,12 @@
+// Last updated: 18/07/2026, 21:27:28
+1class Solution {
+2    public String rearrangeString(String s, char x, char y) {
+3        StringBuilder a = new StringBuilder(), b = new StringBuilder(), c = new StringBuilder();
+4        for(char ch : s.toCharArray()){
+5            if(ch == y) a.append(ch);
+6            else if(ch == x) c.append(ch);
+7            else b.append(ch);
+8        }
+9        return a.append(b).append(c).toString();
+10    }
+11}
