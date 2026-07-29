5cf1a3bfb3bcfa596342f219e23805b7ff17a6dc
Tharanirajan M <tharanirajantharani1610@gmail.com>
2026-07-29 14:43:59 +0530

    Create 97-InterleavingString

---
+++ 97-InterleavingString/version/java/97-InterleavingString_20260729_144400.java
@@ -0,0 +1,32 @@
+// Last updated: 29/07/2026, 14:44:00
+1
+2class Solution {
+3    public List<List<Integer>> levelOrder(TreeNode root) {
+4        List<List<Integer>> result = new ArrayList<>();
+5        if (root == null) return result;
+6
+7        Queue<TreeNode> queue = new LinkedList<>();
+8        queue.add(root);
+9
+10        while (!queue.isEmpty()) {
+11            int levelSize = queue.size();
+12            List<Integer> currentLevel = new ArrayList<>();
+13
+14            for (int i = 0; i < levelSize; i++) {
+15                TreeNode currentNode = queue.poll();
+16                currentLevel.add(currentNode.val);
+17
+18                if (currentNode.left != null) {
+19                    queue.add(currentNode.left);
+20                }
+21                if (currentNode.right != null) {
+22                    queue.add(currentNode.right);
+23                }
+24            }
+25
+26            result.add(currentLevel);
+27        }
+28
+29        return result;
+30    }
+31}
